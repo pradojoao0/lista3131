324971c8c312d913034d1439958eafaa667e6c7c
João Prado <60278803+pradojoao0@users.noreply.github.com>
2022-08-18 21:32:40 -0300

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+
+def lerMatriz():
+    m, ler_linha = [], input()
+    while ler_linha: 
+        m.append([int(i) for i in ler_linha.split(" ") if i])
+        ler_linha = input()
+    return m 
+def escreverMatriz(m):
+    for linha in m:
+        print(*linha)
+
+
+matriz = lerMatriz();
+
+colunas = len(matriz[0])
+linhas = len(matriz)
+
+##Definir a linha do meio 
+
+cont = 1
+
+for linha in range(0, len(matriz)):
+    for coluna in range(0, len(matriz[linha])):
+        if(coluna >= linha):
+            break
+        else:
+            if(matriz[linha][coluna] % 2 != 0):
+                cont = cont*matriz[linha][coluna]
+
+print(f"multiplicação abaixo ímpares = {cont}")
