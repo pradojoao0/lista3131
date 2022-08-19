c5a60a0119e4d40357123c07570b2ad14efea13f
João Prado <60278803+pradojoao0@users.noreply.github.com>
2022-08-18 22:21:45 -0300

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -2,10 +2,10 @@
 
 def lerMatriz():
     m, ler_linha = [], input()
-    while ler_linha: 
+    while ler_linha:
         m.append([int(i) for i in ler_linha.split(" ") if i])
         ler_linha = input()
-    return m 
+    return m
 def escreverMatriz(m):
     for linha in m:
         print(*linha)
@@ -27,17 +27,17 @@ for linha in range(0, linhasA):
         if(linha % 2 == 0):
             if(coluna % 2 == 0):
                 if(matrizA[linha][coluna] % 2 == 0):
-                    multPreta *= matrizA[linha][coluna]
+                    multBranca *= matrizA[linha][coluna]
             else:
                 if(matrizA[linha][coluna] % 2 == 0):
-                    multBranca *= matrizA[linha][coluna]
+                    multPreta *= matrizA[linha][coluna]
         else:
             if(coluna % 2 == 0):
                 if(matrizA[linha][coluna] % 2 == 0):
-                    multBranca *= matrizA[linha][coluna]
+                    multPreta *= matrizA[linha][coluna]
             else:
                 if(matrizA[linha][coluna] % 2 == 0):
-                    multPreta *= matrizA[linha][coluna]
+                    multBranca *= matrizA[linha][coluna]
 
-print(f"multiplicação pares na cor=0 é {multPreta}; e na outra cor é {multBranca}")
+print(f"multiplicação pares na cor=1 é {multPreta}; e na outra cor é {multBranca}")
 
