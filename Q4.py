1b401b5f00ab749ea1a9519dc4fea48a9ad4c1ed
João Prado <60278803+pradojoao0@users.noreply.github.com>
2022-08-18 21:33:22 -0300

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def lerMatriz():
+    m, ler_linha = [], input()
+    while ler_linha: 
+        m.append([int(i) for i in ler_linha.split(" ") if i])
+        ler_linha = input()
+    return m 
+def escreverMatriz(m):
+    for linha in m:
+        print(*linha)
+        
+        
+m = lerMatriz()
+
+linhas = len(m)
+colunas = len(m[0])
+
+gotcha = False
+
+if(colunas >= 13):
+    
+    for l in range(0, linhas):
+        if(gotcha == False):
+            for c in range(0, colunas):
+                if(m[l][13] == 5):
+                    print(l)
+                    gotcha = True
+                    break
+        else:
+            break
+                    
+                
+            
+    if(gotcha == False):
+        print("-1")
+            
+else:
+    print("-1")
